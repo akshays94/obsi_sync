61dade834b00cfd9820e0489a38d19fc90e8d91d
Akshay Suresh <akshaysuresh_t@rollingarrays.com>
2023-04-24 10:33:29 +0530

    ADDED: obsi_sync.py

---
+++ obsi_sync.py
@@ -0,0 +1,51 @@
+import os
+import logging
+from datetime import datetime
+
+LOG_LEVEL = logging.DEBUG
+
+FORMAT = '[%(levelname)s:%(name)s:%(asctime)s] %(message)s'
+formatter = logging.Formatter(FORMAT)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('obsi_sync.log')
+file_handler.setFormatter(formatter)
+
+log = logging.getLogger('osynclogger')
+log.setLevel(LOG_LEVEL)
+
+log.addHandler(console_handler)
+log.addHandler(file_handler)
+
+
+def main():
+
+    log.debug('STARTED')
+
+    log.debug('BUILDING COMMAND')
+
+    cmd = f"{get_repo()} \
+        git add -A; \
+        git commit -am '{get_commit_msg()}'; \
+        git pull origin master; \
+        git push -u origin master;"
+
+    log.debug(f'RUNNING -> {cmd}')
+
+    os.system(cmd)
+
+    log.debug('COMPLETED')
+
+
+def get_repo():
+    return "cd ~/Sync; cd \"Akie's Vault\";"
+
+
+def get_commit_msg():
+    return f'OBS: {datetime.strftime(datetime.now(), "%d-%m-%Y %H:%M")}'
+
+
+if __name__ == '__main__':
+    main()
