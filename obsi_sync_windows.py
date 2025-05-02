5cfe4a83209632b670faf27d86b472b179b290cc
Akshay Thekkath <akshaysuresh1994@gmail.com>
2025-05-02 19:47:01 +0530

    ADDED: obsi sync windows

--- obsi_sync_windows.py
+++ obsi_sync_windows.py
@@ -21,30 +21,32 @@ log.addHandler(file_handler)
 
 
 def main():
-
-    log.debug("STARTED")
+    log.debug("STARTED FOR WINDOWS")
 
     log.debug("BUILDING COMMAND")
 
-    cmd = f"{get_repo()} \
-        git add -A; \
-        git commit -am '{get_commit_msg()}'; \
-        git pull origin master; \
-        git push -u origin master;"
-
+    cmd = f'{get_repo()} && git add -A && git commit -am "{get_commit_msg()}"'
     log.debug(f"RUNNING -> {cmd}")
-
     os.system(cmd)
 
-    log.debug("COMPLETED")
+    cmd = f"{get_repo()} && git pull origin master"
+    log.debug(f"RUNNING -> {cmd}")
+    os.system(cmd)
+
+    cmd = f"{get_repo()} && git push -u origin master"
+    log.debug(f"RUNNING -> {cmd}")
+    os.system(cmd)
+
+    log.debug("COMPLETED FOR WINDOWS")
 
 
 def get_repo():
-    return "cd ~/projects/akies-obsidian;"
+    # /d allows changing drives (important if you’re not on D:)
+    return r"cd /d D:\akie_projects\akies-obsidian"
 
 
 def get_commit_msg():
-    return f'OBS: {datetime.strftime(datetime.now(), "%d-%m-%Y %H:%M")}'
+    return f'OBS: WINDOWS {datetime.strftime(datetime.now(), "%d-%m-%Y %H:%M")}'
 
 
 if __name__ == "__main__":
